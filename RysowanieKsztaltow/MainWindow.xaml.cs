5eea278a2096745c0f33d03a22426723d1d2efe7
Damian Matyjaszek <tougepl@gmail.com>
2017-07-01 19:37:14 +0200

    Initial commit

---
+++ RysowanieKsztaltow/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using d = System.Drawing;
+using System.IO;
+
+namespace RysowanieKsztaltow
+{
+    public partial class MainWindow : Window
+    {
+        private byte[] pixs;
+        private int pixsW, pixsH;
+        private Rysownik rysownik;
+        private Point p;
+        private double dpi;
+        private double skala;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dpi = 96;
+            skala = 0.8;
+            Loaded += delegate { ResetScreen(); };
+        }
+
+        private void ResetScreen()
+        {
+            pixsW = (int)(Ramka.ActualWidth * skala);
+            pixsH = (int)(Ramka.ActualHeight * skala);
+            pixs = new byte[4 * pixsW * pixsH];
+            rysownik = new Rysownik(ref pixs, pixsW, pixsH);
+            Screen.Source = BitmapSource.Create(pixsW, pixsH, dpi, dpi, PixelFormats.Bgra32, null, pixs, 4 * pixsW);
+        }
+
+        private void Screen_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            p = new Point(e.GetPosition(Screen).X, e.GetPosition(Screen).Y);
+        }
+
+        private void Screen_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                rysownik.CzyscEkran();
+                //rysownik.RysujLinie((int)(p.X * skala), (int)(p.Y * skala), (int)(e.GetPosition(Screen).X * skala), (int)(e.GetPosition(Screen).Y * skala));
+                rysownik.RysujKolo((int)(p.X * skala), (int)(p.Y * skala), (int)(e.GetPosition(Screen).X * skala), (int)(e.GetPosition(Screen).Y * skala));
+                Screen.Source = BitmapSource.Create(pixsW, pixsH, dpi, dpi, PixelFormats.Bgra32, bmp, pixs, 4 * pixsW);
+            }
+        }
+
+        private void Screen_MouseLeave(object sender, MouseEventArgs e)
+        {
+            //var img = d.Image.FromFile("spectrum.bmp");
+            //using (var ms = new MemoryStream())
+            //{
+            //    img.Save(ms, d.Imaging.ImageFormat.Bmp);
+            //    byte[] pixs = ms.ToArray();
+            //    Screen.Source = BitmapSource.Create(img.Width, img.Height, dpi, dpi, PixelFormats.Bgr32, null, pixs, img.Width * 4);
+
+            //}
+        }
+
+        private void Screen_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ResetScreen();
+        }
+    }
+}
---
+++ RysowanieKsztaltow/Rysowink.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace RysowanieKsztaltow
+{
+    class Rysownik
+    {
+        private byte kolorR, kolorG, kolorB, alpha;
+        private byte[] pixs;
+        private int wysokosc, szerokosc;
+
+        public Rysownik(ref byte[] pixs, int szerokosc, int wysokosc)
+        {
+            kolorR = kolorG = kolorB = 0;
+            alpha = 255;
+            this.pixs = pixs;
+            this.szerokosc = szerokosc;
+            this.wysokosc = wysokosc;
+        }
+
+        public void UstawPedzel(byte r, byte g, byte b, byte a)
+        {
+            kolorR = r;
+            kolorG = g;
+            kolorB = b;
+            alpha = a;
+        }
+
+        public void RysujPiksel(int x, int y)
+        {
+            if (x >= 0 && x < szerokosc && y >= 0 && y < wysokosc)
+            {
+                int pozycja = 4 * (y * szerokosc + x);
+                pixs[pozycja] = kolorB;
+                pixs[pozycja + 1] = kolorG;
+                pixs[pozycja + 2] = kolorR;
+                pixs[pozycja + 3] = alpha;
+            }
+        }
+
+        public void CzyscEkran()
+        {
+            for (int i = 0; i < pixs.Length; ++i)
+            {
+                pixs[i] = 255;
+            }
+        }
+
+        public void RysujLinie(int x0, int y0, int x1, int y1)
+        {
+            int startX = Math.Min(x0, x1);
+            int endX = Math.Max(x0, x1);
+            int startY = Math.Min(y0, y1);
+            int endY = Math.Max(y0, y1);
+            int dx = endX - startX;
+            int dy = endY - startY;
+
+            if (dx > dy)
+            {
+                for (int x = startX; x <= endX; ++x)
+                {
+                    double y = (dy / (double)dx) * (x - x0) + y0;
+
+                    if ((x1 > x0 && y1 > y0) || (x1 < x0 && y1 < y0))
+                        RysujPiksel(x, (int)Math.Floor(y));
+                    else
+                        RysujPiksel(x, 2 * y0 - (int)Math.Floor(y));
+                }
+            }
+            else
+            {
+                for (int y = startY; y <= endY; ++y)
+                {
+                    double x = (dx / (double)dy) * (y - y0) + x0;
+
+                    if ((x1 > x0 && y1 > y0) || (x1 < x0 && y1 < y0))
+                        RysujPiksel((int)Math.Floor(x), y);
+                    else
+                        RysujPiksel(2 * x0 - (int)Math.Floor(x), y);
+                }
+            }
+        }
+
+        public void RysujKolo(int x0, int y0, int x1, int y1)
+        {
+            int r;
+
+            if (x0 > x1) r = x0 - x1;
+            else r = x1 - x0;
+
+            for (int x = 0; x <= r; ++x)
+            {
+                int y = (int)Math.Floor(Math.Sqrt(r * r - x * x));
+
+                // Prawy góra
+                RysujPiksel(y + x0, -x + y0);
+                RysujPiksel(x + x0, -y + y0);
+
+                // Lewy góra
+                RysujPiksel(-y + x0, -x + y0);
+                RysujPiksel(-x + x0, -y + y0);
+
+                // Prawy dół
+                RysujPiksel(y + x0, x + y0);
+                RysujPiksel(x + x0, y + y0);
+
+                // Lewy dół
+                RysujPiksel(-y + x0, x + y0);
+                RysujPiksel(-x + x0, y + y0);
+            }
+        }
+    }
+}
+
